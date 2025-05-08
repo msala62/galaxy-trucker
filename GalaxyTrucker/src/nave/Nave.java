56bd8c36d695c3fed6180fa7bc7d14cc5b687825
PaoloAngeloRadaelli <p.radaelli3@studenti.unibg.it>
2025-05-08 13:32:41 +0200

    Nuovo metodo stampaNave e eliminaCargo

--- GalaxyTrucker/src/nave/Nave.java
+++ GalaxyTrucker/src/nave/Nave.java
@@ -32,27 +32,63 @@ public class Nave {
 		return this.plancia;
 	}
 
-	public void stampa() {
-		String RESET = "\u001B[0m";
-		String GREEN = "\u001B[32m";
-		String YELLOW = "\u001B[33m";
+	/*	public void stampa() {
+	String RESET = "\u001B[0m";
+	String GREEN = "\u001B[32m";
+	String YELLOW = "\u001B[33m";
 
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 7; j++) {
-				if (this.plancia[i][j].utilizzabile) {
-					System.out.print(GREEN + this.plancia[i][j].getComponente() + "\t" + RESET);
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 7; j++) {
+			if (this.plancia[i][j].utilizzabile) {
+				System.out.print(GREEN + this.plancia[i][j].getComponente() + "\t" + RESET);
+			} else {
+				if ((i == 0 && j == 5) || (i == 0 && j == 6)) {
+					System.out.print(YELLOW + this.plancia[i][j].getComponente() == null ? this.plancia[i][j] : this.plancia[i][j].getComponente() + "\t" + RESET);
 				} else {
-					if ((i == 0 && j == 5) || (i == 0 && j == 6)) {
-						System.out.print(YELLOW + this.plancia[i][j].getComponente() == null ? this.plancia[i][j] : this.plancia[i][j].getComponente() + "\t" + RESET);
-					} else {
-						System.out.print("  \t");
-					}
+					System.out.print("  \t");
 				}
-
 			}
-			System.out.println("");
+
 		}
+		System.out.println("");
 	}
+}*///Lasciata in forma di commento nel caso la versione sottostante non funzionasse
+
+public void stampa() 
+{
+	String RESET = "\u001B[0m";
+	String GREEN = "\u001B[32m";
+	String YELLOW = "\u001B[33m";
+	
+	StringBuilder sbSopra = new StringBuilder();//Per connettore SU
+	StringBuilder sbMezzo = new StringBuilder();//Per connettori SX DX e nome componente
+	StringBuilder sbSotto = new StringBuilder();//Per connettore GIU
+	
+	for (int i = 0; i < 7; i++)//Per mostrare le colonne
+		System.out.print(YELLOW + "/t" + i + "/t" + RESET);
+	
+	for (int i = 0; i < 5; i++) 
+	{
+		sbMezzo.append(YELLOW + i + RESET);//Per mostrare le righe
+		sbMezzo.append("/t");
+		for (int j = 0; j < 7; j++) 
+		{
+			if (this.plancia[i][j].utilizzabile)//Se la casella è utilizzabile si cerca il componente associato e ogni sua parte viene appended allo stringbuilder associato. TODO cosa fare se casella è utilizzabile, ma vuota
+			{
+				sbSopra.append(GREEN + "/t" + this.plancia[i][j].getComponente().getConnettoreSU() + "/t" + RESET);
+				sbMezzo.append(GREEN + this.plancia[i][j].getComponente().getConnettoreSX() + "/t" + this.plancia[i][j].getComponente().nomeComponente() + "/t" + this.plancia[i][j].getComponente().getConnettoreDX() + RESET);
+				sbSotto.append(GREEN + "/t" + this.plancia[i][j].getComponente().getConnettoreGIU() + "/t" + RESET);
+			}
+			else//Se la casella non è utilizzabile si appenda due tabulazioni
+			{
+				sbSopra.append("/t/t");
+				sbMezzo.append("/t/t");
+				sbSotto.append("/t/t");
+			}
+		}
+		System.out.println(sbSopra + "/n" + sbMezzo + "/n" + sbSotto + "/n");//Si printa una riga di componenti alla volta
+	}
+}
 
 	//IMPORTANTE: Y sono le righe e X le colonne
 	public boolean aggiungiComponente(int y, int x, Componente tessera) {
@@ -267,6 +303,8 @@ public class Nave {
 		return false;
 	}
 
+	//TODO Funzione per scambiare merce tra due stive.
+	//TODO overhaul anche di questa funzione. Assegnazione automatica o manuale?
 	public boolean caricaCargo(List<Cargo> cargo) {
 		if (cargo.isEmpty())
 			return false;
@@ -288,9 +326,175 @@ public class Nave {
 		return cargo.isEmpty();
 	}
 	
-	public boolean eliminaCargo(List<Cargo> cargo) {
-		//TODO
-		return cargo.isEmpty();
+	//Per eliminare un certo numero di merci partendo dalle più costose, come nel caso si venisse derubati da pirati o contrabbandieri
+	public boolean eliminaCargo(int cargoDaPerdere) {
+		
+		//I primi due for loop setacciano l'intera nave per istanze di componenti stiva
+		for (int i = 0; i < plancia.length; i++) 
+		{
+			for (int j = 0; j < plancia[0].length; j++) 
+			{
+				if (cargoDaPerdere <= 0)
+					return true;
+
+				if (isUtilizzabile(this.plancia[i][j]) && this.plancia[i][j].getComponente() instanceof Stiva) 
+				{
+					Stiva stivaSpeciale = (Stiva) this.plancia[i][j].getComponente();
+					//Dato che si perde merci a partire dalle più costose si cercano per prima le merci rosse
+					while (stivaSpeciale.isSpeciale() && cargoDaPerdere > 0 && stivaSpeciale.getCargoCorrente().size() > 0 ) 
+					{
+						//Anche le stive speciali possono trasportare merce normale, dunque per ogni merce in stiva si cerca se è rosso, se sì viene rimossa
+						for (Cargo merce : stivaSpeciale.getCargoCorrente()) 
+						{
+							if(merce.getColore() == ColoreCargo.ROSSO) 
+							{
+								stivaSpeciale.getCargoCorrente().remove(merce);
+								cargoDaPerdere--;
+							}
+							
+							//Se è abbastanza per saldare il debito si esce dalla funzione
+							if (cargoDaPerdere <= 0)
+								return true;
+						}
+					}
+				}
+			}
+		}
+
+		//Si passa poi alle merci gialle procedendo nella stessa maniera di prima
+		for (int i = 0; i < plancia.length; i++) 
+		{
+			for (int j = 0; j < plancia[0].length; j++) 
+			{
+				if (cargoDaPerdere <= 0)
+					return true;
+
+				if (isUtilizzabile(this.plancia[i][j]) && this.plancia[i][j].getComponente() instanceof Stiva) 
+				{
+					Stiva stiva = (Stiva) this.plancia[i][j].getComponente();
+					
+					while (cargoDaPerdere > 0 && stiva.getCargoCorrente().size() > 0 ) 
+					{
+						
+						for (Cargo merce : stiva.getCargoCorrente()) 
+						{
+							if(merce.getColore() == ColoreCargo.GIALLO) 
+							{
+								stiva.getCargoCorrente().remove(merce);
+								cargoDaPerdere--;
+							}
+							
+							//Se è abbastanza per saldare il debito si esce dalla funzione
+							if (cargoDaPerdere <= 0)
+								return true;
+						}
+					}
+				}
+			}
+		}
+		
+		//Si passa poi alle merci verdi procedendo nella stessa maniera di prima
+		for (int i = 0; i < plancia.length; i++) 
+		{
+			for (int j = 0; j < plancia[0].length; j++) 
+			{
+				if (cargoDaPerdere <= 0)
+					return true;
+
+				if (isUtilizzabile(this.plancia[i][j]) && this.plancia[i][j].getComponente() instanceof Stiva) 
+				{
+					Stiva stiva = (Stiva) this.plancia[i][j].getComponente();
+					
+					while (cargoDaPerdere > 0 && stiva.getCargoCorrente().size() > 0 ) 
+					{
+						
+						for (Cargo merce : stiva.getCargoCorrente()) 
+						{
+							if(merce.getColore() == ColoreCargo.VERDE) 
+							{
+								stiva.getCargoCorrente().remove(merce);
+								cargoDaPerdere--;
+							}
+							
+							//Se è abbastanza per saldare il debito si esce dalla funzione
+							if (cargoDaPerdere <= 0)
+								return true;
+						}
+					}
+				}
+			}
+		}
+		
+		//Si passa poi alle merci blu procedendo nella stessa maniera di prima
+		for (int i = 0; i < plancia.length; i++) 
+		{
+			for (int j = 0; j < plancia[0].length; j++) 
+			{
+				if (cargoDaPerdere <= 0)
+					return true;
+
+				if (isUtilizzabile(this.plancia[i][j]) && this.plancia[i][j].getComponente() instanceof Stiva) 
+				{
+					Stiva stiva = (Stiva) this.plancia[i][j].getComponente();
+					
+					while (cargoDaPerdere > 0 && stiva.getCargoCorrente().size() > 0 ) 
+					{
+						
+						for (Cargo merce : stiva.getCargoCorrente()) 
+						{
+							if(merce.getColore() == ColoreCargo.BLU) 
+							{
+								stiva.getCargoCorrente().remove(merce);
+								cargoDaPerdere--;
+							}
+							
+							//Se è abbastanza per saldare il debito si esce dalla funzione
+							if (cargoDaPerdere <= 0)
+								return true;
+						}
+					}
+				}
+			}
+		}
+		
+		//Se non si riesce a trovare abbastanza merci da togliere, si passa ai segnalini batteria
+		for (int i = 0; i < plancia.length; i++) 
+		{
+			for (int j = 0; j < plancia[0].length; j++) 
+			{
+				if (cargoDaPerdere <= 0)
+					return true;
+
+				if (isUtilizzabile(this.plancia[i][j]) && this.plancia[i][j].getComponente() instanceof Batteria) 
+				{
+					Batteria batteria = (Batteria) this.plancia[i][j].getComponente();
+					
+					while (cargoDaPerdere > 0 && batteria.getCarica() > 0 ) 
+					{
+						//Si tolgono quanti più segnalini si può da ciascun componente batteria 
+						for (int h = cargoDaPerdere; h > 0; h--) 
+						{
+							if(batteria.getCarica() > 0) 
+							{
+								batteria.scalaCarica();
+								cargoDaPerdere--;
+							}
+							else 
+							{
+								break;
+							}
+							
+							//Se è abbastanza per saldare il debito si esce dalla funzione
+							if (cargoDaPerdere <= 0)
+								return true;
+						}
+					}
+				}
+			}
+		}
+		
+		//Se dopo tutto questo il debito non è ancora saldato si esce dalla funzione
+		return false;
 	}
 
 	public int getPotenzaMotrice() {
