cbdf0189e6c1ca5c66c638f09a8872d85bddac58
alesh24 <a.sajid@studenti.unibg.it>
2025-05-17 15:56:27 +0200

    Update GestisciAttacchi.java

--- GalaxyTrucker/src/planciavolo/GestisciAttacchi.java
+++ GalaxyTrucker/src/planciavolo/GestisciAttacchi.java
@@ -1,5 +1,21 @@
 package planciavolo;
 
+/**
+ * L'interfaccia GestisciAttacchi definisce   la gestione degli attacchi
+ * nel gioco. Le classi che implementano questa interfaccia devono fornire un'implementazione
+ * del metodo 'gestisciAttacco'.
+ */
 public interface GestisciAttacchi {
-	String gestisciAttacco(String tipoAttacco, boolean hasBordiLisci, boolean scudoAttivo, int potenzaCannoni, String parteColpita);
+	
+	/**
+     * Gestisce un attacco specifico nel gioco e determina il suo esito.
+     * @param tipoAttacco    (es., "meteoriti", "cannonate").
+     * @param hasBordiLisci  Se la nave attaccata ha bordi lisci (per deviare piccoli meteoriti).
+     * @param scudoAttivo    Se lo scudo della nave attaccata è attivo.
+     * @param potenzaCannoni La potenza dei cannoni della nave attaccante (per distruggere grossi meteoriti).
+     * @param parteColpita   La parte della nave colpita ( a causa dell'attacci pesanti).
+     * @return               Una stringa che descrive l'esito dell'attacco.
+     */
+	String gestisciAttacco(String tipoAttacco, boolean hasBordiLisci, boolean scudoAttivo, 
+			int potenzaCannoni, String parteColpita);
 }
